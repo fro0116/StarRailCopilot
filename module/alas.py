cc62bbc65cd72614f821def0ba63afca6d3f5313
fro0116 <18626656+fro0116@users.noreply.github.com>
2024-11-23 06:58:53 +0900

    Add go home on empty

--- module/alas.py
+++ module/alas.py
@@ -216,6 +216,14 @@ class AzurLaneAutoScript:
                         continue
                     if task.command != 'Restart':
                         self.run('start')
+                elif method == 'goto_home':
+                    logger.info('Goto home screen during wait')
+                    self.run('goto_home')
+                    release_resources()
+                    self.device.release_during_wait()
+                    if not self.wait_until(task.next_run):
+                        del_cached_property(self, 'config')
+                        continue
                 elif method == 'goto_main':
                     logger.info('Goto main page during wait')
                     self.run('goto_main')
